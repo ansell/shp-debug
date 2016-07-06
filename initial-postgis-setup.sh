12bd6aab51bdd2d92f6e4c88bf888b34a47c43eb
Peter Ansell <p_ansell@yahoo.com>
2016-07-06 19:26:08 +1000

    add script for initially setting up postgis

---
+++ initial-postgis-setup.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+set -e
+set -x
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+PGUSER="gisuser"
+PGPASSWORD="something"
+PGDB="test_postgis_db"
+PGDB_TEMPLATE="template_postgis2.2"
+PGHOST="localhost"
+
+POSTGRES_JDBC_CONNECTION="jdbc:postgresql://${PGHOST}/${PGDB}?user=${PGUSER}&password=${PGPASSWORD}"
+POSTGRES_PSQL_CONNECTION="host=${PGHOST} user=${PGUSER} dbname=${PGDB} password=${PGPASSWORD}"
+POSTGRES_PSQL_TEMPLATE_CONNECTION="host=${PGHOST} user=${PGUSER} dbname=${PGDB_TEMPLATE} password=${PGPASSWORD}"
+
+sudo -u postgres dropdb --if-exists ${PGDB}
+sudo -u postgres dropdb --if-exists ${PGDB_TEMPLATE}
+sudo -u postgres dropuser --if-exists ${PGUSER}
+
+sudo -u postgres createuser --pwprompt --superuser ${PGUSER}
+sudo -u postgres createdb -O ${PGUSER} ${PGDB_TEMPLATE}
+
+psql "${POSTGRES_PSQL_TEMPLATE_CONNECTION}" -c "CREATE EXTENSION postgis;"
+
+cd /tmp 
+wget https://raw.githubusercontent.com/pedrogit/postgisaddons/master/postgis_addons.sql
+psql "${POSTGRES_PSQL_TEMPLATE_CONNECTION}" -a -f postgis_addons.sql
+rm postgis_addons.sql
+cd
+
+psql "${POSTGRES_PSQL_TEMPLATE_CONNECTION}" -c "SELECT postgis_version();"
+
+sudo -u postgres createdb -T ${PGDB_TEMPLATE} ${PGDB}
+
+psql "${POSTGRES_PSQL_CONNECTION}" -c "ALTER DATABASE \"${PGDB}\" SET postgis.gdal_enabled_drivers TO 'ENABLE_ALL';"
